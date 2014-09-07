ace1fe8862f21b4406571b3daf71c36fa2336971
Jacob Yates <jacobyates@LA-CPS828325MN-Huk.local>
2014-09-07 14:41:32 -0500

    no more core requirements

--- +pdsa/interpThresh.m
+++ +pdsa/interpThresh.m
@@ -8,11 +8,13 @@ function [thresh, x, y] = interpThresh(inference, level)
 % 	   thresh [double] - stimulus value that reaches [level] performance
 % 	     fitX  [m x 1] - vector of stimulus values
 % 		 fitY  [m x 1] - vector of PMF values (for plotting)
-% requirements:
-% 	inference.core 	  MUST be set to 'ab' for this to work
-%   inference.sigmoid MUST be 'logistic' or 'gauss'
+
+
 
 % 20140825 jly 	wrote it
+% 20140907 jly  use psignifit pmf diagnostics so there are no 'core'
+%               requirements. Uses only unique values so the function is
+%               always monotonic.
 
 import pdsa.* % matters if running from pdstools
 
@@ -26,25 +28,55 @@ if nargin < 2 || isempty(level)
 	end
 end
 
-switch inference.sigmoid
-	case 'logistic'
-		sigmoid	= 'logistic';
-	case 'gauss'
-		sigmoid = 'norm';
-	otherwise
-		error('sigmoid must be logistic or gaussian')
+
+% Diagnostics of the point estimate
+if inference.gammaislambda
+    diag = Diagnostics ( inference.data, inference.params_estimate, ...
+        'sigmoid', inference.sigmoid, 'core', inference.core, 'nafc', inference.nafc, 'cuts', inference.cuts, 'gammaislambda' );
+else
+    diag = Diagnostics ( inference.data, inference.params_estimate, ...
+        'sigmoid', inference.sigmoid, 'core', inference.core, 'nafc', inference.nafc, 'cuts', inference.cuts );
 end
 
-assert(strcmp(inference.core, 'ab'), 'run psignifit with "core" set to "ab"')
+x = diag.pmf(:,1);
+y = diag.pmf(:,2);
 
-x = linspace(0, 1, 1e3);
-y = cdf(sigmoid, x, inference.params_estimate(1), inference.params_estimate(2));
-
-switch inference.nafc
-	case 1
-		y = inference.params_estimate(4) + (1 - inference.params_estimate(3) - inference.params_estimate(4))*y;
-	case 2
-		y = .5 + (.5 - inference.params_estimate(3))*y;
+% if things aren't monotonic because it saturates too early
+if numel(unique(y)) < numel(y)
+    uy = unique(y);
+    nu = numel(unique(y));
+    yInd = zeros(nu,1);
+    for ii = 1:nu
+        [~,yInd(ii)] = find(y==uy(ii), 1, 'first');
+    end
+    y = y(yInd);
+    x = x(yInd);
 end
-
 thresh = interp1(y, x, level);
+
+
+% old version with core ab requirements
+% requirements:
+% 	inference.core 	  MUST be set to 'ab' for this to work
+%   inference.sigmoid MUST be 'logistic' or 'gauss'
+
+% switch inference.sigmoid
+% 	case 'logistic'
+% 		sigmoid	= 'logistic';
+% 	case 'gauss'
+% 		sigmoid = 'norm';
+% 	otherwise
+% 		error('sigmoid must be logistic or gaussian')
+% end
+% 
+% assert(strcmp(inference.core, 'ab'), 'run psignifit with "core" set to "ab"')
+% 
+% x = linspace(0, 1, 1e3);
+% y = cdf(sigmoid, x, inference.params_estimate(1), inference.params_estimate(2));
+% 
+% switch inference.nafc
+% 	case 1
+% 		y = inference.params_estimate(4) + (1 - inference.params_estimate(3) - inference.params_estimate(4))*y;
+% 	case 2
+% 		y = .5 + (.5 - inference.params_estimate(3))*y;
+% end
