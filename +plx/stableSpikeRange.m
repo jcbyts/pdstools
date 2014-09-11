946d40bd589792a8cae8b0a2a84dd2ce348b166f
Jacob Yates <jacobyates@LA-CPS828325MN-Huk.local>
2014-09-10 22:36:45 -0500

    tool for exploring stability of neural recordings

--- +plx/stableSpikeRange.m
+++ +plx/stableSpikeRange.m
@@ -1,10 +1,35 @@
-function spikes = stableSpikeRange(spikes)
+function [spikes, h] = stableSpikeRange(spikes, getInput)
 % find range with stable recordings
 
+if nargin < 2
+    getInput = false;
+end
 units = unique(spikes.id);
 nUnits = numel(units);
 
+h = nan(nUnits,1);
+if isfield(spikes, 'goodRange')
+    for ii = 1:nUnits
+        stimes = spikes.time(spikes.id==units(ii));
+        bs = 30; % seconds
+        bins = 0:bs:max(stimes);
+        spcnt = histc(stimes, bins)/bs;
+        x = [bins(:) ones(numel(bins),1)];
+        wls = (x'*x)\(x'*spcnt);
+        rhat = x*wls;
+        h(ii) = figure(ii); clf; hold all
+        plot(bins, spcnt, 'o', bins, rhat, 'r')
+        xlabel('time (seconds)')
+        ylabel('spike rate')
+        title(sprintf('unit: %d', units(ii)))
+        idx = bins>spikes.goodRange(units(ii),1) & bins<spikes.goodRange(units(ii),2);
+        plot(bins(idx), spcnt(idx), 'r.')
+    end
+   return 
+end
+
 spikes.goodRange = nan(numel(spikes.snr),2);
+
 for ii = 1:nUnits
     stimes = spikes.time(spikes.id==units(ii));
     bs = 30; % seconds
@@ -14,9 +39,8 @@ for ii = 1:nUnits
     wls = (x'*x)\(x'*spcnt);
     rhat = x*wls;
     resids = spcnt - rhat; 
-   figure(1); clf; hold all
-    plot(bins, spcnt, 'o', bins, rhat, 'r')
-%     plot(bins, zscore(spcnt), 'o')
+    h(ii) = figure(ii); clf; hold all
+	plot(bins, spcnt, 'o', bins, rhat, 'r')
 
     aboveThreshold = resids<-(std(spcnt)/2);
     spanLocs = bwlabel(aboveThreshold);   %identify contiguous ones
@@ -34,7 +58,15 @@ for ii = 1:nUnits
     goodSpikes = find(spanLocs==id);
 %     goodSpikes = find(spanLocs==1);
     plot(bins(goodSpikes), spcnt(goodSpikes), 'r.');
-    spikes.goodRange(units(ii),:) = [bins(goodSpikes(1)) bins(goodSpikes(end))];
-
-    pause
+    if getInput
+        tmp = input('enter spike range as a vector');
+    end
+    if isempty(tmp) || ~(numel(tmp)==2)
+        spikes.goodRange(units(ii),:) = [bins(goodSpikes(1)) bins(goodSpikes(end))];
+    else
+        spikes.goodRange(units(ii),:) = tmp;
+    end
+    xlabel('time (seconds)')
+    ylabel('spike rate')
+    title(sprintf('unit: %d', units(ii)))
 end
