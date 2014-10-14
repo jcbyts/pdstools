d58b5ee579f60e42fdab53e7721b9181fda1ed4a
Jacob Yates <jacobyates@LA-CPS828325MN-Huk.local>
2014-10-13 23:13:17 -0500

    check for analysis is working

--- +ov/checkForAnalysis.m
+++ +ov/checkForAnalysis.m
@@ -1,22 +1,80 @@
-function analysisRecord = checkForAnalysis(entity, Name, Param)
+function analysisRecord = checkForAnalysis(entity, protocolName, Param)
 % check for analysis by protocol and protocol parameters instead of by name
 % UNDER DEVELOPMENT
-anRecs = ov.getAnalysis(entity);
+% anRecs = ov.getAnalysis(entity);
+% anRecs(1).getName
 
-anRecs(1).getName
+import ovation.*
 
-Name = 'Population PSTH';
-arName = [Name '_' date];
+analysisRecord = [];
 
-pr = context.getOrInsertProtocol(Name, '');
-protocol = pr.get;
+context = entity.getDataContext();
+entity = context.refreshEntity(entity);
 
-nAnRecs = numel(anRecs);
+projs = ovation.asarray(entity.getProjects);
+project = projs(1);
 
-for 
-singleNeuronPhys = ov.getAnalysis(entity, 'Single Neuron Physiology');
+assert(numel(project) == 1, 'only one project works here')
 
-ar = entity.addAnalysisRecord(arName,...
-        anRecs(1),...
-        protocol,... % no protocol
+pr = context.getOrInsertProtocol(protocolName, [protocolName '.m']);
+
+if pr.isNew
+    fprintf('New Protocol Created\r')
+end
+
+% build query options
+paramFields = fieldnames(Param);
+nParams = numel(paramFields);
+values = cell(2,nParams);
+for ii = 1:nParams
+    values{1,ii} = paramFields{ii};
+    tmp = Param.(paramFields{ii});
+    switch class(tmp)
+        case 'char'
+            values{2,ii} = tmp;
+        case 'double'
+            values{2,ii} = num2str(tmp);
+    end
+end
+
+searchStr = sprintf('param:%s=%s && ', values{:});
+searchStr(end-3:end) = [];
+
+% Search for all AnalysisRecords with parameters (note boolean logic)
+results = ovation.query(context, searchStr);
+nResults = numel(results);
+
+% Iterate the results and check each record for protocol name and project
+for ii = 1:nResults
+    obj = results(ii);
+    if (~isempty(obj) && strcmp(char(obj.getClass().getSimpleName()), 'AnalysisRecord'))
+        if(strcmp(char(obj.getProtocol().getName()), protocolName))
+            if(obj.getParent().equals(project))
+                disp('Found it!');
+                analysisRecord = obj;
+                return
+            end
+        end
+    end
+end
+
+if isempty(analysisRecord)
+    makeNew = input('do you want to add the analysis record?');
+
+    if makeNew
+        arName   = [protocolName '_' date];
+        protocol = pr.get;
+        an = ov.getAnalysis(project,1);
+        output = an.getOutputs;
+        
+        
+        analysisRecord = project.addAnalysisRecord(arName,...
+        output,...
+        protocol,...
         ovation.struct2map(Param));
+    else
+        return
+    end
+else
+    return
+end
