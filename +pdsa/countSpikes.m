a53de8994be29e169c705093e5f8a7b8ac1edc39
Jacob Yates <jacobyates@la-cps884733md-huk.psy.utexas.edu>
2015-10-02 11:30:33 -0500

    count spikes simplified

--- +pdsa/countSpikes.m
+++ +pdsa/countSpikes.m
@@ -1,4 +1,4 @@
-function [v, id, dt] = countSpikes(sptimes, ev1, ev2)
+function [cnt,dt] = countSpikes(sptimes, ev1, ev2)
 % Count spikes between two events
 % v = countSpikes(sptimes, ev1, ev2)
 % input
@@ -7,62 +7,30 @@ function [v, id, dt] = countSpikes(sptimes, ev1, ev2)
 %       ev2 [(p or 1) x 1] - vector of event times to stop count or
 %                            scalar window size to count
 % output
-%       v  - vector of spike counts
-%       id - index of ev1 that count comes from
+%       cnt - vector of spike counts
+%       dt  - vector of time windows
 
 % 20140416 jly wrote it
 
-
-validEventStarts = find(~isnan(ev1));
-validEventStops  = find(~isnan(ev2));
 nE1 = numel(ev1);
 nE2 = numel(ev2);
 
+assert(nE2==1 | nE2==nE1, 'event 2 must have same # as event 1, or be a window size')
+
 if nE2 == 1 && nE1~=nE2
-    e1 = ev1(validEventStarts);
-    e2 = e1+ev2;
-elseif nE1 == nE2
-    e1 = ev1;
-    e2 = ev2;
-else
-    error('you need to make them the same size')
-    e1   = ev1(validEventStarts);
-    tmp  = ev2(validEventStops);
-    e2   = nan(numel(e1),1);
-    for ii = 1:nE1
-        if ii == nE1
-            if tmp(ii) > e1(ii)
-                e2(ii) = tmp(ii);
-            else
-                e1(ii) = nan;
-            end
-            break
-        end
-        if ii == nE2
-            break
-        end
-        eid = find(tmp>e1(ii) & tmp<e1(ii+1), 1);
-        if ~isempty(eid)
-            e2(ii) = tmp(eid);
-        else
-            e1(ii) = nan;
-        end
-    end
-    
+    ev2 = ev1+ev2;
 end
 
-% good = ~isnan(e1);
-good = true(size(e1));
-e1 = e1(good);
-e2 = e2(good);
-nE1 = sum(good);
-dt = e2-e1;
-id = [];
-% id = validEventStarts(good);
-v = nan(nE1,1);
-validIdx = intersect(validEventStarts, validEventStops);
-nValid = numel(validIdx);
-for jj = 1:nValid %     1:nE1
-    ii = validIdx(jj);
-   v(ii) = sum(sptimes > e1(ii) & sptimes < e2(ii));
+invalidEvents=isnan(ev1)|isnan(ev2);
+
+cnt=nan(nE1,1);
+dt=nan(nE1,1);
+
+for ii = 1:nE1
+    if invalidEvents(ii)
+        continue
+    end
+    
+    cnt(ii)=sum(sptimes>ev1(ii) & sptimes<ev2(ii));
+    dt(ii)=ev2(ii)-ev1(ii);
 end
