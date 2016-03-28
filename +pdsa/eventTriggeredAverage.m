fc8631664fdc4bf3b28161bf05c4426f3925c462
Jacob Yates <jacobyates@la-cps884733md-huk.psy.utexas.edu>
2016-03-28 17:30:08 -0500

    event Triggered average won't change number of event times even if they don't have enough data.

--- +pdsa/eventTriggeredAverage.m
+++ +pdsa/eventTriggeredAverage.m
@@ -20,15 +20,20 @@ windowSize = numel(widx);
 stAn    = zeros(windowSize, size(an, 2));
 stAn_SD = zeros(windowSize, size(an, 2));
 
-st(isnan(st)) = []; % remove impossible spikes
-st(st > size(an,1)) = [];
-
-widxs = bsxfun(@plus, st, widx);
-widxs(widxs(:,1) <= 0,:) = []; % ignore very early spikes
-widxs(widxs(:,end) > size(an,1),:) = []; % ignore very late spikes
+validEvents=find(~(isnan(st) | st > size(an,1)));
+% st(isnan(st)) = []; % remove impossible spikes
+% st(st > size(an,1)) = [];
+n=numel(st);
+widxs = bsxfun(@plus, st(validEvents), widx);
+bad=widxs(:,1) <= 0;
+validEvents(bad)=[];
+widxs(bad,:) = []; % ignore very early spikes
+bad=widxs(:,end) > size(an,1);
+validEvents(bad)=[];
+widxs(bad,:) = []; % ignore very late spikes
 spidx = all(widxs,2);
 if nargout >= 4
-    wfs = zeros(size(widxs,1), numel(widx)*size(an,2));
+    wfs = nan(n, numel(widx)*size(an,2));
 end
     
 for kCh = 1:size(an,2)
@@ -37,6 +42,6 @@ for kCh = 1:size(an,2)
     stAn(:, kCh) = nanmean(l);
     stAn_SD(:, kCh) = nanstd(l); %/ sqrt(size(l,1));
     if nargout >=4
-        wfs(:,(1:numel(widx)) + numel(widx) * (kCh-1)) = l;
+        wfs(validEvents,(1:numel(widx)) + numel(widx) * (kCh-1)) = l;
     end
 end
